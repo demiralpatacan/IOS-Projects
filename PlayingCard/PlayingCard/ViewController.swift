836e4b01fb3b9e6b8449ca258413f6b15b16feb1
zv6661 <atacandemiralp@Atacan-MacBook-Pro.local>
2018-07-14 23:05:16 +0200

    PlayingCard Game first upload

---
+++ PlayingCard/PlayingCard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PlayingCard
+//
+//  Created by Atacan on 14.07.2018.
+//  Copyright © 2018 Atacan Demiralp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for _ in 1...10 {
+            if let card = deck.draw() {
+                print("\(card)")
+            }
+        }
+    }
+
+
+}
+
